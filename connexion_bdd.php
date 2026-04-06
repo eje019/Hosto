f74e93fd7483dfa4fe180b9a84d0bc93cd669af5
eje019 <emricndayake@gmail.com>
2026-04-06 08:19:40 +0100

    Fix : New en new et correction des commentaires

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,13 +1,15 @@
 <?php
 try {
-    $pdo = NEW PDO("mysql : host = localhost, dbname= hosto", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=hosto", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch(PDOException $e){
-    die("Erreur de connexion a ka base de donnees");
+    die("Erreur de connexion a la base de donnees");
 }
 ?>
 
-//new PDO : on ouvre une nouvelle connexion
-//dans cette nouvelle connexion on renseigne le nom du serveur : localhost , le nom de la base de donnees , le nom d'utilisateur sans mdp par defaut<?php
-//on le met dans un try, catch pour afficher un messsage d'erreur clair si la connexion echoue
+<!-- 
+new PDO : on ouvre une nouvelle connexion
+dans cette nouvelle connexion on renseigne le nom du serveur : localhost , le nom de la base de donnees , le nom d'utilisateur sans mdp par defaut
+on le met dans un try, catch pour afficher un messsage d'erreur clair si la connexion echoue 
+-->
